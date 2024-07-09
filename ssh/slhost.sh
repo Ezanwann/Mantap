0d4e03dac451264c562c39841c61cc2612f6b581
Ezanwann <wannhafizan9@gmail.com>
2024-07-09 18:29:35 +0800

    Update slhost.sh added cf key

--- ssh/slhost.sh
+++ ssh/slhost.sh
@@ -24,7 +24,7 @@ DOMAIN=iniluna.xyz
 SUB_DOMAIN=kongsi.iniluna.xyz
 NS_DOMAIN=ns.kongsi.inipuna.xyz
 CF_ID=ezanstream@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+CF_KEY=4ca048bc011061b0e01f2e4a0bdf535e038a8
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${SUB_DOMAIN}..."
