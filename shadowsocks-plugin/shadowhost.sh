7b6fb19802262d6a23fa85276ddb8cfff2d08f4d
Ezanwann <wannhafizan9@gmail.com>
2024-07-09 19:36:35 +0800

    Update shadowhost.sh

--- shadowsocks-plugin/shadowhost.sh
+++ shadowsocks-plugin/shadowhost.sh
@@ -20,11 +20,11 @@ rm -rf /var/lib/crot/ipvps.conf
 #
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=slowdns-${subsl}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=3dcb550a77cb0a64dacf205243e3ac550f9db
+DOMAIN=iniluna.xyz
+SUB_DOMAIN=kongsi.iniluna.xyz
+NS_DOMAIN=ns.kongsi.iniluna.xyz
+CF_ID=aloneloner21@gmail.com
+CF_KEY=
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/ipvps.conf
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
